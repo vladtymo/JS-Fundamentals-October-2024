a20fdf0194dc58c1c2dc775c18eda9d4990ad03f
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-07 18:10:47 +0200

    Node_js server example

---
+++ 15 Node.js/second.js
@@ -0,0 +1,17 @@
+// load module: require(module_name)
+require("./first");
+
+const utils = require('./examples/utils');
+
+function welcome(name) {
+    console.log(`----- ${global.title} -----`);
+    console.log("Hello, ", name);
+}
+
+welcome("Vlad");
+
+console.log("Test title: ", title);
+
+utils.showMessage("tymovlad");
+utils.showMessage(); // username
+console.log(utils.user);
